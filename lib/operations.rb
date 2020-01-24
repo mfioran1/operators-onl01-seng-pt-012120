568620db0bb17b1930171588e2ab01b21bd6f5bc
Maurice Fiorani <mauricef97@gmail.com>
2020-01-24 16:54:02 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,17 +2,18 @@ def unsafe?(speed)
   if speed > 60
     true
   elsif speed < 40
-    true
-  else
-    false
-  end
+  true
+else
+  false
+end
 
 end
 
 
 
 def not_safe?(speed)
-  if speed ? 
+speed > 60 && speed <40 ? true : false
+end
 	
 end
 	
